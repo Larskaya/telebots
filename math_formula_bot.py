2c806b837a7ee2628580cf1f9af477b84aeec6d3
elena <elena@MacBook-Air-elena.local>
2019-03-31 16:55:18 +0300

    one function

--- math_formula_bot.py
+++ math_formula_bot.py
@@ -3,29 +3,6 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(commands=["start"])
-def get_formula_name(message):
-	sent = bot.send_message(message.chat.id, '/SokrUmn - сокращенного умножения \n/SinCosTgCtg - sin cos tg ctg \n/Plosh - площади \n/Deskr - дескриминант и корни уравнения')
-	bot.register_next_step_handler(sent, all_formuls)
-
-def all_formuls(message):
-	print(message.text, 'mes txt')
-	if message.text == '/SokrUmn':
-		print(1)
-		sent = bot.send_message(message.chat.id, '/SokrUmnForm \n 2 f') # варианты
-		bot.register_next_step_handler(sent, get_formula_sokr_umn)
-	elif message.text == '/SinCosTgCtg':
-		print(2)
-		sent = bot.send_message(message.chat.id, '/SinCosTgCtg - sin,cos,tg,cts \n/TableSinCosTgCtg - таблица sin cos tg ctg')
-		bot.register_next_step_handler(sent, get_formula_sin_cos)
-	elif message.text == '/Plosh':
-		print(3)
-		sent = bot.send_message(message.chat.id, '/kvadr - квадрат \n/pryam - прямоугольник \n/treug - прямоугольный треугольник \n/krug - круг \n/romb - ромб \n/parallelep - параллелепипед \n/parallelog - параллелограмм')
-		bot.register_next_step_handler(sent, get_formula_plosh)
-	elif message.text == '/Deskr':
-		sent = bot.send_message(message.chat.id, '/deskr - дескриминант \n/korn - корни уравнения')
-		bot.register_next_step_handler(sent, get_formula_deskr)
-
 sokr_umn_img = open('f1.png', 'rb')
 sin_cos_img = open('sin-cos-tg-ctg.png', 'rb')
 table_sin_img = open('table-sin-cos-tg-ctg.png', 'rb')
@@ -45,12 +22,10 @@ korni_img = open('korni.png', 'rb')
 formuls_image_sokr = {
 	'/SokrUmnForm': sokr_umn_img,
 }
-
 formuls_image_sin_cos = {
 	'/SinCosTgCtg': sin_cos_img,
 	'/TableSinCosTgCtg': table_sin_img,
 }
-
 formuls_image_plosh = {
 	'/kvadr': plKv_img,
 	'/pryam': plPryam_img,
@@ -60,31 +35,38 @@ formuls_image_plosh = {
 	'/parallelog': plParallelog_img,
 	'/treug': plPryamTreug_img,
 }
-
 formuls_image_deskr = {
 	'/deskr': deskr_img,
 	'/korn': korni_img
 }
 
-def get_formula_sokr_umn(message):
-	if message.text in formuls_image_sokr:
-		sent = bot.send_photo(message.chat.id, formuls_image_sokr[message.text])
-		bot.register_next_step_handler(sent, all_formuls)
+@bot.message_handler()
+def get_formula_name(message):
+	if message.text == '/start':
+		bot.send_message(message.chat.id, '/sokrUmn - сокращенного умножения \n/sinCosTgCtg - sin cos tg ctg \n/plosh - площади \n/deskr - дескриминант и корни уравнения')
+	elif message.text == '/sokrUmn':
+		bot.send_message(message.chat.id, '/SokrUmnForm \n 2 f') # варианты
+	elif message.text == '/sinCosTgCtg':
+		bot.send_message(message.chat.id, '/SinCosTgCtg - sin,cos,tg,cts \n/TableSinCosTgCtg - таблица sin cos tg ctg')
+	elif message.text == '/plosh':
+		bot.send_message(message.chat.id, '/kvadr - квадрат \n/pryam - прямоугольник \n/treug - прямоугольный треугольник \n/krug - круг \n/romb - ромб \n/parallelep - параллелепипед \n/parallelog - параллелограмм')
+	elif message.text == '/deskr':
+		bot.send_message(message.chat.id, '/deskr - дескриминант \n/korn - корни уравнения')
 
-def get_formula_sin_cos(message):
-	if message.text in formuls_image_sin_cos:
-		sent = bot.send_photo(message.chat.id, formuls_image_sin_cos[message.text])
-		bot.register_next_step_handler(sent, all_formuls)
+	elif message.text in formuls_image_sokr:
+		bot.send_photo(message.chat.id, formuls_image_sokr[message.text])
 
-def get_formula_plosh(message):
-	if message.text in formuls_image_plosh:
-		sent = bot.send_photo(message.chat.id, formuls_image_plosh[message.text])
-		bot.register_next_step_handler(sent, all_formuls)
+	elif message.text in formuls_image_sin_cos:
+		bot.send_photo(message.chat.id, formuls_image_sin_cos[message.text])
 
-def get_formula_deskr(message):
-	if message.text in formuls_image_deskr:
-		sent = bot.send_photo(message.chat.id, formuls_image_deskr[message.text])
-		bot.register_next_step_handler(sent, all_formuls)
+	elif message.text in formuls_image_plosh:
+		bot.send_photo(message.chat.id, formuls_image_plosh[message.text])
+
+	elif message.text in formuls_image_deskr:
+		bot.send_photo(message.chat.id, formuls_image_deskr[message.text])
+
+	else:
+		bot.send_message(message.chat.id, 'приветствую')
 
 bot.polling()
 
